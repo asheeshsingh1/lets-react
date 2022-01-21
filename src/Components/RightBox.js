2dd5efb44215927c24d163192f1341b93af914b5
asheeshsingh1 <93409596+asheeshsingh1@users.noreply.github.com>
2022-01-22 01:17:41 +0530

    Basic Struct 1

---
+++ src/Components/RightBox.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+export default function RightBox() {
+  return (
+      <>
+      {/* <h5 className='yummy'>
+      Yummy Cart
+      </h5> */}
+      <div className='cart common'>
+            <h5>Yummy Cart</h5>
+          {/* Hey I am a Div. */}
+      </div>
+      <button className='checkout btn common' >
+          Yummy  <h1> 😋😋</h1>
+      </button>
+      </>
+  );
+}
